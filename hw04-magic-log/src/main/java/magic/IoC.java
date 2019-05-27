f1543199e5d8a15279443e7bbec14039f15b9bcc
YanSavash <savash.yan@mail.ru>
2019-05-27 12:49:51 +0300

    Final resultSet

--- hw04-magic-log/src/main/java/magic/IoC.java
+++ hw04-magic-log/src/main/java/magic/IoC.java
@@ -5,6 +5,8 @@ import Annotations.Log;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.HashMap;
+import java.util.Map;
 
 public class IoC {
 
@@ -15,21 +17,26 @@ public class IoC {
         return (TestLoggingInterface) Proxy.newProxyInstance(IoC.class.getClassLoader(), new Class<?>[]{TestLoggingInterface.class}, handler);
     }
 
+    private static Map<String, Method> logMethods = new HashMap<>();
 
     static class MagicInvocHandler implements InvocationHandler {
         private final TestLoggingInterface magicClass;
 
         MagicInvocHandler(TestLoggingInterface myClass) {
             this.magicClass = myClass;
+            for (Method m: magicClass.getClass().getDeclaredMethods())
+                if (m.isAnnotationPresent(Log.class)) {
+                    Log lg = m.getAnnotation(Log.class); //я знаю, что данная проверка необязательна - я для себя, практикую возможности аннотаций))
+                    if (lg.mark().equals("true"))
+                        logMethods.put(m.getName(), m);
+                }
         }
 
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            if (method.isAnnotationPresent(Log.class)) {
-                Log lg = method.getAnnotation(Log.class);
-                if (lg.mark().equals("true"))
-                    System.out.println("executed method:" + method.getName() + " param: " + args[0]);
+            if (logMethods.containsKey(method.getName())) {
+                System.out.println("executed method:" + method.getName() + " param: " + args[0]);
             }
             return method.invoke(magicClass, args);
         }
