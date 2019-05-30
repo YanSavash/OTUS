7c59c3a9e3c5082928550e61991f4c95e6c192da
YanSavash <savash.yan@mail.ru>
2019-05-30 10:51:48 +0300

    Research

---
+++ hw05-testing-different-gc/src/main/java/OutOfMemoryList.java
@@ -0,0 +1,31 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class OutOfMemoryList {
+
+    public static void main(String[] args){
+        run();
+    }
+
+    public static void run(){
+        List<List> memory = new LinkedList();
+        for (int i = 0; i < Integer.MAX_VALUE; i++){
+            memory.add(makeBrick());
+            System.out.println(i);
+            //Label_1
+//            try {
+//                Thread.sleep(10);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+        }
+    }
+
+    public static List makeBrick(){
+        List<Integer> brick = new LinkedList<>();
+        for (int i = 0; i < 1000; i++)
+            brick.add(i);
+        return brick;
+    }
+
+}
