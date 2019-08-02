aece0e20b03a5364d375c5735c82056054b86322
YanSavash <savash.yan@mail.ru>
2019-08-02 11:26:15 +0300

    Threads

---
+++ hw14-threads/src/main/java/ru/netrax/Main.java
@@ -0,0 +1,52 @@
+package ru.netrax;
+
+public class Main {
+    private final Object monitor = new Object();
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.makeRerun();
+    }
+
+    private void rerun() {
+        while (true) {
+            inc();
+            dec();
+        }
+    }
+
+    private void inc() {
+        try {
+            for (int i = 1; i < 10; i++) {
+                synchronized (monitor) {
+                    System.out.println(Thread.currentThread().getName() + " " + i);
+                    Thread.sleep(500);
+                }
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void dec() {
+        try {
+            for (int i = 10; i > 1; i--) {
+                synchronized (monitor) {
+                    System.out.println(Thread.currentThread().getName() + " " + i);
+                    Thread.sleep(500);
+                }
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void makeRerun() {
+        Thread thread1 = new Thread(this::rerun);
+        Thread thread2 = new Thread(this::rerun);
+        thread1.start();
+        thread2.start();
+    }
+}
